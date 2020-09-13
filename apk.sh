813f3beae2ac6eb360e305ff7810baeb8b89e7f5
Urlam <tim@mangobilling.com>
2020-09-13 12:52:18 +0530

    new changes

--- apk.sh
+++ apk.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo ".......... Pushing .........."
+echo ".......... ..........  Pushing .......... .........."
 git add .
 git commit -m "new changes"
 git push
-echo ".......... Pushing is Done.........."
-echo "Deleting www .tmp .sourcemaps."
+echo ".......... .......... Pushing is Done .......... .........."
+echo ".......... .......... Deleting www .tmp .sourcemaps. .......... .........."
 rm -rf www .tmp .sourcemaps
+echo ".......... .......... Deleting done. .......... .........."
 ionic cordova build android --prod --release
+echo ".......... .......... APK Created. .......... .........."
 EOF
