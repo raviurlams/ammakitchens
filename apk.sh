6ef995f80d525e55f064602c748cdd243ef27cbb
Urlam <tim@mangobilling.com>
2020-09-13 12:58:16 +0530

    new changes

--- apk.sh
+++ apk.sh
@@ -7,6 +7,5 @@ echo ".......... .......... Pushing is Done .......... .........."
 echo ".......... .......... Deleting www .tmp .sourcemaps. .......... .........."
 rm -rf www .tmp .sourcemaps
 echo ".......... .......... Deleting done. .......... .........."
-ionic cordova build android --prod --release
+ionic cordova build android --prod
 echo ".......... .......... APK Created. .......... .........."
-EOF
