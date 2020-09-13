2799ede0d1fbc3095b9710a1c48a2bce2478da36
Urlam <tim@mangobilling.com>
2020-09-13 12:33:30 +0530

    new changes

--- apk.sh
+++ apk.sh
@@ -6,5 +6,5 @@ git push
 echo ".......... Pushing is Done.........."
 echo "Deleting www .tmp .sourcemaps."
 rm -rf www .tmp .sourcemaps
-ionic cordova emulate android
+ionic cordova build android --prod --release
 EOF
