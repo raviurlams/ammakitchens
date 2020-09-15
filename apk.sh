73d90aa50228bcbd415c1c88989c37d3eacc483e
Urlam <tim@mangobilling.com>
2020-09-15 23:33:22 +0530

    new changes

--- apk.sh
+++ apk.sh
@@ -6,7 +6,8 @@ git push
 ionic cap sync
 echo ".......... .......... Pushing is Done .......... .........."
 echo ".......... .......... Deleting www .tmp .sourcemaps. .......... .........."
-rm -rf www .tmp .sourcemaps
+rm -rf www .tmp .sourcemaps node_modules
+npm install
 echo ".......... .......... Deleting done. .......... .........."
 ionic cordova build android --prod
 echo ".......... .......... APK Created. .......... .........."
