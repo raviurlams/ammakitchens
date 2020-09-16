7fe3e9dab5f06586bba1cf73ccf77a5041d20282
Urlam <tim@mangobilling.com>
2020-09-16 12:25:10 +0530

    ui

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { SmsRetriever } from '@ionic-native/sms-retriever/ngx';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+declare var SMS: any;
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.page.html',
@@ -9,7 +13,8 @@ export class RegistrationPage implements OnInit {
   ionicForm: FormGroup;
   defaultDate = "1980-05-05";
   isSubmitted = false;
-  constructor(public formBuilder: FormBuilder) { }
+  hash: any;
+  constructor(public formBuilder: FormBuilder, private smsRetriever: SmsRetriever,) { }
 
   ngOnInit() {
     this.initializeForm();
@@ -24,6 +29,7 @@ export class RegistrationPage implements OnInit {
       customerDOB: [this.defaultDate, [Validators.required]],
       customerDOJ: [new Date(), [Validators.required]],
       customerGender: ['m'],
+      customerAddressProof: ['', [Validators.required, Validators.minLength(5)]],
 
     })
   }
@@ -45,7 +51,40 @@ export class RegistrationPage implements OnInit {
       console.log('Please provide all the required values!')
       return false;
     } else {
-      console.log(this.ionicForm.value)
+      let otp = Math.floor(100000 + Math.random() * 900000);
+      this.sendMessage('+91' + this.ionicForm.value.customerPhNumber, otp);
     }
   }
+
+  retriveSMS() {
+    console.log('Watching SMS');
+    this.smsRetriever.startWatching()
+      .then((res: any) => {
+        const otp = res.Message.toString().substr(4, 6);
+        alert(`OTP Received - ${otp}`);
+      })
+      .catch((error: any) => console.error(error));
+  }
+
+  genHash() {
+    // This function is to get hash string of APP.
+    // * @return {Promise<string>} Returns a promise that resolves when successfully generate hash of APP.
+    this.smsRetriever.getAppHash()
+      .then((res: any) => {
+        console.log(res);
+        //alert('hash : '+res);
+        this.hash = res;
+      })
+      .catch((error: any) => console.error(error));
+  }
+
+  sendMessage(number, otp) {
+    SMS.sendSMS(number, '<#> ' + otp + ' is your 6 digit OTP from Amma Kitchens', () => {
+      this.genHash();
+      this.retriveSMS();
+    }, (error) => {
+      alert(error);
+    });
+  }
+
 }
